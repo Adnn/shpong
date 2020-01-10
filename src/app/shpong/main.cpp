7721b1ab40c7018de3ac113e501e27fb3b5b44cb
Adrien <Adnn@users.noreply.github.com>
2020-01-10 15:21:04 +0100

    Uses enginer Application class to remove main.cpp boilerplate

--- src/app/shpong/main.cpp
+++ src/app/shpong/main.cpp
@@ -1,127 +1,49 @@
-#include <renderer/Error.h>
-
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-
-#include <iostream>
-#include <stdexcept>
+#include <engine/Application.h>
+#include <engine/Engine.h>
+#include <engine/Timer.h>
 
 #include "game.h"
 
 constexpr int gWindowWidth{1280};
 constexpr int gWindowHeight{1024};
 
-constexpr int gGLVersionMajor{4};
-constexpr int gGLVersionMinor{1};
-
-
-static void error_callback(int error, const char* description)
+int main(int argc, const char * argv[])
 {
-    std::cerr << "GLFW error: " << description << std::endl;
-}
-
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
-{
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GLFW_TRUE);
-}
-
-static void windowsSize_callback(GLFWwindow * window, int width, int height)
-{
-    ad::Engine * engine = static_cast<ad::Engine *>(glfwGetWindowUserPointer(window));
-    engine->callbackWindowSize(width, height);
-}
-
-int main(void)
-{
-    glfwSetErrorCallback(error_callback);
-    if (!glfwInit())
+    try
     {
-        throw std::runtime_error("Unable to initialize glfw");
-    }
-    Guard glfwInitGuard{glfwTerminate};
+        ad::Application application("Shpong", gWindowWidth, gWindowHeight);
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, gGLVersionMajor);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, gGLVersionMinor);
+        ad::Timer timer{glfwGetTime(), 0.};
+        std::unique_ptr<ad::Scene> scene = ad::setupScene(*application.mEngine);
+        ad::MouseInput mouse;
 
-    // macOS requirement (as well as not going above to OpenGL 4.1)
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-    // Only show the window after its size callback is set
-    // so we cannot miss notifications
-    glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
-
-    auto window = guard(glfwCreateWindow(gWindowWidth,
-                                         gWindowHeight,
-                                         "Shpong",
-                                         NULL,
-                                         NULL),
-                        glfwDestroyWindow);
-    if (!window)
-    {
-        throw std::runtime_error("Unable to initialize window or context");
-    }
-
-    glfwMakeContextCurrent(window);
-    gladLoadGL();
-
-    ad::Engine engine;
-    glfwSetWindowUserPointer(window, &engine);
-    // Explicitly call it, because it is used to complete the engine setup
-    {
-        // Get the size, because the hints might not be satisfied
-        // (yet not invoking the size callback)
-        int width, height;
-        glfwGetWindowSize(window, &width, &height);
-        windowsSize_callback(window, width, height);
-    }
-
-    glfwSetKeyCallback(window, key_callback);
-    glfwSetWindowSizeCallback(window, windowsSize_callback);
-
-    glfwShowWindow(window);
-
-    // VSync
-    glfwSwapInterval(1);
-
-    if (!GLAD_GL_KHR_debug)
-    {
-        std::cerr << "Debug output is not available."
-                  << " Please run on a decent platform for debugging."
-                  << std::endl;
-    }
-    else
-    {
-        ad::enableDebugOutput();
-    }
-
-    std::unique_ptr<ad::Scene> scene = ad::setupScene(engine);
-    ad::Timer timer{glfwGetTime(), 0.};
-    ad::MouseInput mouse;
-
-    while(!glfwWindowShouldClose(window))
-    {
+        while(application.nextFrame())
         {
-            double x, y;
-            glfwGetCursorPos(window, &x, &y);
-            bool isClicked = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT)==GLFW_PRESS;
+            {
+                double x, y;
+                glfwGetCursorPos(application.mWindow, &x, &y);
+                bool isClicked =
+                    glfwGetMouseButton(application.mWindow, GLFW_MOUSE_BUTTON_LEFT)==GLFW_PRESS;
 
-            // Origin is top-left in glfw, and bottom-left in OpenGL: convert to OpenGL
-            mouse.mPosition = {static_cast<GLfloat>(x),
-                               static_cast<GLfloat>(engine.getWindowSize().height() - y)};
-            mouse.mClickEdge = (!mouse.mClick) && isClicked;
-            mouse.mReleaseEdge = mouse.mClick && !isClicked;
-            mouse.mClick = isClicked;
+                // Origin is top-left in glfw, and bottom-left in OpenGL: convert to OpenGL
+                mouse.mPosition = {static_cast<GLfloat>(x),
+                                   static_cast<GLfloat>(application.mEngine->getWindowSize().height() - y)};
+                mouse.mClickEdge = (!mouse.mClick) && isClicked;
+                mouse.mReleaseEdge = mouse.mClick && !isClicked;
+                mouse.mClick = isClicked;
+            }
+            ad::updateScene(*scene, *application.mEngine, timer, mouse);
+            ad::renderScene(*scene, *application.mEngine);
+
+            timer.mark(glfwGetTime());
         }
-
-        ad::updateScene(*scene, engine, timer, mouse);
-        ad::renderScene(*scene, engine);
-
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-
-        timer.mark(glfwGetTime());
+    }
+    catch(const std::exception & e)
+    {
+        std::cerr << "Exception:\n"
+                  << e.what()
+                  << std::endl;
+        std::exit(EXIT_FAILURE);
     }
 
     std::exit(EXIT_SUCCESS);
